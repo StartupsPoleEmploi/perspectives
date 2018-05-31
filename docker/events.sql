bbf876e751893d8b67054d3d7f564cfe07128988
Brice Friederich <friederich.brice@gmail.com>
2018-05-31 11:09:39 +0200

    - integration de la base de données dans le fichier docker - configuration du logback pour la recette - ajout du script SQL pour initialiser la base

---
+++ docker/events.sql
@@ -0,0 +1,21 @@
+-- Table: events
+
+-- DROP TABLE IF EXISTS events;
+
+CREATE TABLE events
+(
+  id bigserial NOT NULL,
+  stream_version integer NOT NULL,
+  event_data jsonb NOT NULL,
+  event_type character varying(255),
+  stream_name character varying(255) NOT NULL,
+  CONSTRAINT events_pk PRIMARY KEY (id),
+  UNIQUE (stream_version, stream_name)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE events OWNER TO perspectives;
+COMMENT ON TABLE events IS 'Contient les événements';
+COMMENT ON COLUMN events.stream_version IS 'version du stream (incrémentale)';
+COMMENT ON COLUMN events.stream_name IS 'Identifiant de l''aggregat concerne par l''événement';
