251e6e2d8d6c0332431ae6bb841ab9e7c3d615dc
Brice Friederich <friederich.brice@gmail.com>
2018-07-17 14:35:10 +0200

    modification du script de backup pour supprimer le conteneur docker démarré automatiquement

--- docker/postgresql/backup-postgres.sh
+++ docker/postgresql/backup-postgres.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-sudo docker run -d --name postgresql-backup --mount src=perspectives_postgresql,target=/var/lib/postgresql/data postgres:10.4
+CONTAINER_ID=`sudo docker run -d --name postgresql-backup --mount src=perspectives_postgresql,target=/var/lib/postgresql/data postgres:10.4`
+
+echo "CONTAINER : $CONTAINER_ID"
 
 sudo docker exec -it postgresql-backup pg_dumpall -c -U perspectives > dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 
 sudo docker container stop postgresql-backup
+
+sudo docker container rm $CONTAINER_ID
