793004abef4f48bafae0fd8e06de7aaf80d873de
Brice Friederich <friederich.brice@gmail.com>
2018-07-18 08:48:43 +0200

    modification du script de restauration (peut executer n'importe quel fichier SQL) pour supprimer le conteneur docker démarré automatiquement

---
+++ docker/postgresql/execute-sql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SQL_FILE="$1"
+
+if [ -z $SQL_FILE ] ; then echo "Préciser le fichier SQL à jouer"; exit ; fi
+
+CONTAINER_ID=`sudo docker run -d --name postgresql-execute --mount src=perspectives_postgresql,target=/var/lib/postgresql/data postgres:10.4`
+
+echo "CONTAINER : $CONTAINER_ID"
+
+cat $SQL_FILE | sudo docker exec -i postgresql-execute psql -U perspectives
+
+sudo docker container stop postgresql-execute
+
+sudo docker container rm $CONTAINER_ID
--- docker/postgresql/restore-postgres.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat dump_05-06-2018_17_49_50.sql | sudo docker exec -i postgresql-backup psql -U perspectives
