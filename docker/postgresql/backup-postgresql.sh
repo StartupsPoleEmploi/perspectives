3fd437252bfe6f8b2c908d327a429c4506b3aa45
Brice Friederich <friederich.brice@gmail.com>
2018-08-02 18:29:57 +0200

    un seul script de backup de base mais deux fichiers generes

--- docker/postgresql/backup-postgresql.sh
+++ docker/postgresql/backup-postgresql.sh
@@ -6,6 +6,8 @@ echo "CONTAINER : $CONTAINER_ID"
 
 sudo docker exec -it postgresql-backup pg_dump -U perspectives -t candidats_cv > dump-candidats-cv-`date +%d-%m-%Y"_"%H_%M_%S`.sql
 
+sudo docker exec -it postgresql-backup pg_dump -U perspectives -T candidats_cv > dump-sans-candidats-cv-`date +%d-%m-%Y"_"%H_%M_%S`.sql
+
 sudo docker container stop postgresql-backup
 
 sudo docker container rm $CONTAINER_ID
