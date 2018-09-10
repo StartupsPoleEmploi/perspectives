e1c6eb70bdb6204a1d5e0e55afb929139a9b6202
Brice Friederich <friederich.brice@gmail.com>
2018-09-10 16:41:33 +0200

    - simplification du composant de selection des métiers et secteurs d'activité suite aux retours Hotjar - modification du composant checkbox

--- webapp/app/assets/javascripts/pages/candidat/saisieCriteresRecherche.js
+++ webapp/app/assets/javascripts/pages/candidat/saisieCriteresRecherche.js
@@ -3,9 +3,6 @@
 $(document).ready(function () {
     var secteursActivites = $("input[type='checkbox'][name='secteurActivite']");
 
-    // On cache les métiers : on affiche uniquement les secteurs d'activité
-    $(".conteneurMetiers").hide();
-
     $("#rechercheAutreMetier-true").each(function () {
         $('#js-critere-metiers').toggle($(this).prop("checked"));
     });
@@ -14,60 +11,22 @@ $(document).ready(function () {
         $('#js-critere-metiers').toggle($(this).prop("value") === "true");
     });
 
-    // Initialisation des secteurs d'activités
+    // Initialisation des secteurs d'activités : coche la case si tous les métiers sont sélectionnés
     secteursActivites.each(function () {
         var secteurActivite = $(this);
-        var labelSecteurActivite = $("label[for='" + secteurActivite.attr("id") + "']");
-        var compteurSecteurActivite = labelSecteurActivite.find(".compteur-metiers-selectionnes");
-        var nbMetiers = secteurActivite.parent().find("input[type='checkbox'][name='listeMetiersRecherches[]']:checked").length;
 
-        var avecMetiers = nbMetiers > 0;
-        labelSecteurActivite.toggleClass("labelSecteurActivite-sans-focus-avec-metiers", avecMetiers);
-        compteurSecteurActivite.toggle(avecMetiers);
-        compteurSecteurActivite.html(nbMetiers);
+        var metiers = secteurActivite.siblings(".metiers").find("input[type='checkbox'][name='listeMetiersRecherches[]']");
+        var metiersSelectionnes = secteurActivite.siblings(".metiers").find("input[type='checkbox'][name='listeMetiersRecherches[]']:checked");
+        secteurActivite.prop("checked", metiers.length === metiersSelectionnes.length);
     });
 
     secteursActivites.click(function () {
         var clickedSecteurActivite = $(this);
-        var labelClickedSecteurActivite = $("label[for='" + clickedSecteurActivite.attr("id") + "']");
-        var compteurClickedSecteurActivite = labelClickedSecteurActivite.find(".compteur-metiers-selectionnes");
-        var metiersContainer = clickedSecteurActivite.siblings(".conteneurMetiers");
+        var isChecked = clickedSecteurActivite.prop("checked");
 
-        // Tout ce qu'on sait c'est que les métiers sont cachés au démarrage
-        metiersContainer.toggle();
-        var isMetiersVisible = metiersContainer.is(":visible");
-
-        // Compte le nombre de métiers selectionnés pour le secteur d'activité
-        var nbMetiers = clickedSecteurActivite.parent().find("input[type='checkbox'][name='listeMetiersRecherches[]']:checked").length;
-
-        // Gestion du deuxième click sur un même secteur : on affiche le nombre de métiers si le secteur est déselectionné et qu'il y en a plus que zéro, on le cache sinon
-        var isUnselected = !isMetiersVisible && nbMetiers > 0;
-        compteurClickedSecteurActivite.toggle(isUnselected);
-        compteurClickedSecteurActivite.html(nbMetiers);
-
-        // On modifie la classe indiquant une sélection en cours
-        labelClickedSecteurActivite.toggleClass("labelSecteurActivite-avec-focus", isMetiersVisible);
-        labelClickedSecteurActivite.toggleClass("labelSecteurActivite-sans-focus-avec-metiers", isUnselected);
-
-        // On décoche le secteur d'activité précedemment coché si présent
-        $("input[type='checkbox'][name='secteurActivite'][id!='" + clickedSecteurActivite.attr("id") + "']:checked").each(function () {
-            var unclickedSecteurActivite = $(this);
-            var unclickedLabelClickedSecteurActivite = $("label[for='" + unclickedSecteurActivite.attr("id") + "']");
-
-            unclickedSecteurActivite.siblings(".conteneurMetiers").hide();
-            unclickedSecteurActivite.prop("checked", false);
-
-            // On modifie la classe
-            unclickedLabelClickedSecteurActivite.removeClass("labelSecteurActivite-avec-focus");
-
-            // On affiche le compteur si le nombre de métiers du secteur est supérieur à 0
-            var compteurUnclickedSecteurActivite = unclickedLabelClickedSecteurActivite.find(".compteur-metiers-selectionnes");
-            var nbMetiersUnclickedSecteurActivite = unclickedSecteurActivite.parent().find("input[type='checkbox'][name='listeMetiersRecherches[]']:checked").length;
-            compteurUnclickedSecteurActivite.toggle(nbMetiersUnclickedSecteurActivite > 0);
-            compteurUnclickedSecteurActivite.html(nbMetiersUnclickedSecteurActivite);
-
-            unclickedLabelClickedSecteurActivite.toggleClass("labelSecteurActivite-sans-focus-avec-metiers", nbMetiersUnclickedSecteurActivite > 0);
-        })
+        clickedSecteurActivite.siblings(".metiers").find("input[type='checkbox'][name='listeMetiersRecherches[]']").each(function() {
+            $(this).prop("checked", isChecked);
+        });
     });
 
     if (window.FileReader) {
