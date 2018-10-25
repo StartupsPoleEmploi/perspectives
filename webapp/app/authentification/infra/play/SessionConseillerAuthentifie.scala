9aa7bb8b6d937e8136060d931a0e18e3f2574dbd
Brice Friederich <friederich.brice@gmail.com>
2018-10-25 15:42:10 +0200

    mise à jour des conseillers

--- webapp/app/authentification/infra/play/SessionConseillerAuthentifie.scala
+++ webapp/app/authentification/infra/play/SessionConseillerAuthentifie.scala
@@ -9,7 +9,6 @@ object SessionConseillerAuthentifie {
   // FIXME : se base sur l'authentification candidat le temps d'avoir une authentification propre
   val mapCandidatsConseillers = Map(
     "8cfee345-dabc-4830-ac6e-ad70418bfcbf" -> ConseillerId("b3c401e5-78a6-4889-8436-0d40e6b4c7b3"),
-    "9d86be6d-6176-42d3-82a4-b9e10ce49a7d" -> ConseillerId("b3c401e5-78a6-4889-8436-0d40e6b4c7b3"),
     "3a1ff0ac-43af-407e-a49a-719802e6eacf" -> ConseillerId("b3c401e5-78a6-4889-8436-0d40e6b4c7b3"),
     "17a57371-221b-4f97-b290-3fc80c6decdc" -> ConseillerId("b3c401e5-78a6-4889-8436-0d40e6b4c7b3")
   )
