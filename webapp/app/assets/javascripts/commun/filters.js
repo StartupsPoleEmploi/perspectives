099a89e4184028c931bf195700f9403bbdc6c5dd
Brice Friederich <friederich.brice@gmail.com>
2019-01-13 19:09:05 +0100

    fix filter capitalize, seule la premiere lettre est en majuscule

--- webapp/app/assets/javascripts/commun/filters.js
+++ webapp/app/assets/javascripts/commun/filters.js
@@ -1,7 +1,7 @@
 Vue.filter('capitalize', function (value) {
     if (!value) return '';
     value = value.toString();
-    return value.charAt(0).toUpperCase() + value.slice(1);
+    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
 });
 
 Vue.filter('boolean', function (value) {
