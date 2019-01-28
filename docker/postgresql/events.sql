581c5418382529b9056880d5c163948d4415f5b5
Brice Friederich <friederich.brice@gmail.com>
2019-01-28 20:33:39 +0100

    contrainte NOT NULL sur event_type et commentaire de colonne pour event_data

--- docker/postgresql/events.sql
+++ docker/postgresql/events.sql
@@ -2,9 +2,9 @@ CREATE TABLE events
 (
   id bigserial NOT NULL,
   stream_version integer NOT NULL,
-  event_data jsonb NOT NULL,
-  event_type character varying(255),
   stream_name character varying(255) NOT NULL,
+  event_data jsonb NOT NULL,
+  event_type character varying(255) NOT NULL,
   CONSTRAINT events_pk PRIMARY KEY (id),
   UNIQUE (stream_version, stream_name)
 )
@@ -15,5 +15,6 @@ ALTER TABLE events OWNER TO perspectives;
 COMMENT ON TABLE events IS 'Contient les événements';
 COMMENT ON COLUMN events.stream_version IS 'version du stream (incrémentale)';
 COMMENT ON COLUMN events.stream_name IS 'Identifiant de l''aggregat concerne par l''événement';
+COMMENT ON COLUMN events.event_data IS 'Données de l''événement serialisé en JSONB';
 
 CREATE INDEX stream_name_idx ON events (stream_name);
