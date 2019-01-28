3d876f03638773a9daeb44dee67440ac59ea5d76
Brice Friederich <friederich.brice@gmail.com>
2019-01-28 16:30:21 +0100

    modif commentaire de colonne pour la table mrs_habiletes

--- docker/postgresql/mrs_habiletes.sql
+++ docker/postgresql/mrs_habiletes.sql
@@ -11,6 +11,6 @@ WITH (
 OIDS =FALSE
 );
 ALTER TABLE mrs_habiletes OWNER TO perspectives;
-COMMENT ON TABLE mrs_habiletes IS 'Table contenant les habiletés associés à la combinaison (code_rome, code_departement)';
+COMMENT ON TABLE mrs_habiletes IS 'Table contenant les habiletés associées à la combinaison (code_rome, code_departement) pour les MRS';
 COMMENT ON COLUMN mrs_habiletes.code_rome IS 'Code ROME du metier';
 COMMENT ON COLUMN mrs_habiletes.code_departement IS 'Code département : les habiletés diffèrent par département';
