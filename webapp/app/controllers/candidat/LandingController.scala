c6b378f1574a3e8819815c69c9137edc459fd109
Brice Friederich <friederich.brice@gmail.com>
2019-02-07 21:20:59 +0100

    - redirections depuis la landing candidat vers la page d'offres si un candidat est connecté, ou vers la page de recherche candidat si un recruteur est connecte - redirections depuis la landing recruteur vers la page de recherche candidat si un recruteur est connecté, ou vers la page de recherche d'offres si un candidat est connecté - simplification des template scala : pas besoin de gérer un candidat identifiant sur le template recruteur et inversement : la redirection est faite depuis le controleur - suppression de cssClassesBody sur le layout candidat et nettoyage sur les pages de confirmation et de saisie de critères

--- webapp/app/controllers/candidat/LandingController.scala
+++ webapp/app/controllers/candidat/LandingController.scala
@@ -3,10 +3,15 @@ package controllers.candidat
 import authentification.infra.play.{OptionalCandidatAuthentifieAction, OptionalCandidatAuthentifieRequest, OptionalRecruteurAuthentifieAction, OptionalRecruteurAuthentifieRequest}
 import conf.WebAppConfig
 import controllers.AssetsFinder
+import fr.poleemploi.perspectives.commun.infra.play.json.JsonFormats._
+import fr.poleemploi.perspectives.projections.candidat.CandidatQueryHandler
 import fr.poleemploi.perspectives.projections.rechercheCandidat.RechercheCandidatQueryHandler
 import javax.inject._
 import play.api.libs.json.Json
 import play.api.mvc._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @Singleton
 class LandingController @Inject()(cc: ControllerComponents,
@@ -14,19 +19,25 @@ class LandingController @Inject()(cc: ControllerComponents,
                                   implicit val webAppConfig: WebAppConfig,
                                   optionalCandidatAuthentifieAction: OptionalCandidatAuthentifieAction,
                                   optionalRecruteurAuthentifieAction: OptionalRecruteurAuthentifieAction,
+                                  candidatQueryHandler: CandidatQueryHandler,
                                   rechercheCandidatQueryHandler: RechercheCandidatQueryHandler) extends AbstractController(cc) {
 
   def landing(): Action[AnyContent] = optionalRecruteurAuthentifieAction.async { optionalRecruteurAuthentifieRequest: OptionalRecruteurAuthentifieRequest[AnyContent] =>
-    optionalCandidatAuthentifieAction { implicit request: OptionalCandidatAuthentifieRequest[AnyContent] =>
-      if (request.isCandidatAuthentifie)
-        Redirect(routes.OffreController.listeOffres())
-      else
-        Ok(views.html.candidat.landing(
-          recruteurAuthentifie = optionalRecruteurAuthentifieRequest.recruteurAuthentifie,
-          jsData = Json.obj(
-            "secteursActivites" -> rechercheCandidatQueryHandler.secteursProposesDtos
-          )
-        ))
+    optionalCandidatAuthentifieAction.async { implicit optionalCandidatAuthentifieRequest: OptionalCandidatAuthentifieRequest[AnyContent] =>
+        if (optionalCandidatAuthentifieRequest.isCandidatAuthentifie)
+          Future.successful(Redirect(controllers.candidat.routes.OffreController.index()))
+        else if (optionalRecruteurAuthentifieRequest.isRecruteurAuthentifie)
+          Future.successful(Redirect(controllers.recruteur.routes.RechercheCandidatController.index()))
+        else
+          Future {
+            val secteursAvecMetiers = rechercheCandidatQueryHandler.secteursProposes
+            Ok(views.html.candidat.landing(
+              jsData = Json.obj(
+                "secteursActivites" -> secteursAvecMetiers,
+                "metiers" -> secteursAvecMetiers.flatMap(_.metiers)
+              )
+            ))
+          }
     }(optionalRecruteurAuthentifieRequest)
   }
 }
--- webapp/app/controllers/recruteur/LandingController.scala
+++ webapp/app/controllers/recruteur/LandingController.scala
@@ -1,21 +1,29 @@
 package controllers.recruteur
 
-import authentification.infra.play.{OptionalRecruteurAuthentifieAction, OptionalRecruteurAuthentifieRequest}
+import authentification.infra.play.{OptionalCandidatAuthentifieAction, OptionalCandidatAuthentifieRequest, OptionalRecruteurAuthentifieAction, OptionalRecruteurAuthentifieRequest}
 import conf.WebAppConfig
 import controllers.AssetsFinder
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 @Singleton
 class LandingController @Inject()(cc: ControllerComponents,
                                   implicit val assets: AssetsFinder,
                                   implicit val webAppConfig: WebAppConfig,
+                                  optionalCandidatAuthentifieAction: OptionalCandidatAuthentifieAction,
                                   optionalRecruteurAuthentifieAction: OptionalRecruteurAuthentifieAction) extends AbstractController(cc) {
 
-  def landing(): Action[AnyContent] = optionalRecruteurAuthentifieAction { implicit request: OptionalRecruteurAuthentifieRequest[AnyContent] =>
-    if (request.isRecruteurAuthentifie)
-      Redirect(routes.RechercheCandidatController.index())
-    else
-      Ok(views.html.recruteur.landing())
+  def landing(): Action[AnyContent] = optionalRecruteurAuthentifieAction.async { optionalRecruteurAuthentifieRequest: OptionalRecruteurAuthentifieRequest[AnyContent] =>
+    optionalCandidatAuthentifieAction.async { implicit optionalCandidatAuthentifieRequest: OptionalCandidatAuthentifieRequest[AnyContent] =>
+      if (optionalCandidatAuthentifieRequest.isCandidatAuthentifie)
+        Future.successful(Redirect(controllers.candidat.routes.OffreController.index()))
+      else if (optionalRecruteurAuthentifieRequest.isRecruteurAuthentifie)
+        Future.successful(Redirect(controllers.recruteur.routes.RechercheCandidatController.index()))
+      else
+        Future(Ok(views.html.recruteur.landing()))
+    }(optionalRecruteurAuthentifieRequest)
   }
 }
