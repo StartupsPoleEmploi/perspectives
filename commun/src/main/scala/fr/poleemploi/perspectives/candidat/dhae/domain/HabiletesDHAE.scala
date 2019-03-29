4b405cc99239acfba67f21ba40c1c6cb4230db19
Brice Friederich <friederich.brice@gmail.com>
2019-03-29 10:06:10 +0100

    nettoyage package object vide

--- commun/src/main/scala/fr/poleemploi/perspectives/candidat/dhae/domain/HabiletesDHAE.scala
+++ commun/src/main/scala/fr/poleemploi/perspectives/candidat/dhae/domain/HabiletesDHAE.scala
@@ -2,6 +2,11 @@ package fr.poleemploi.perspectives.candidat.dhae.domain
 
 import fr.poleemploi.perspectives.commun.domain.{CodeDepartement, CodeROME, Habilete}
 
+/**
+  * DHAE (Des habiletés à l'emploi), est un projet de PôleEmploi afin de valider des candidats sur des CodeROME mais sans
+  * employeur derrière comme pour une MRS classique. <br />
+  * Les habiletés ne sont pas les mêmes que pour les MRS.
+  */
 case class HabiletesDHAE(codeROME: CodeROME,
                          codeDepartement: CodeDepartement,
                          habiletes: List[Habilete])
