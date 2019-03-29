5f3d72613983f30a4cf63618c2c310d15af0bb66
Brice Friederich <friederich.brice@gmail.com>
2019-03-29 10:07:03 +0100

    nettoyage commentaire

--- commun/src/main/scala/fr/poleemploi/perspectives/candidat/mrs/domain/MRSValideeProcessManager.scala
+++ commun/src/main/scala/fr/poleemploi/perspectives/candidat/mrs/domain/MRSValideeProcessManager.scala
@@ -9,21 +9,11 @@ import scala.concurrent.Future
 
 class MRSValideeProcessManager(candidatCommandHandler: CandidatCommandHandler,
                                referentielMRSCandidat: ReferentielMRSCandidat) extends Projection {
-  /**
-    * @return La liste des classes d'événement à écouter
-    */
+
   override def listenTo: List[Class[_ <: Event]] = List(classOf[CandidatInscritEvent])
 
-  /**
-    * Indique si la projection peut être rejouée
-    */
   override def isReplayable: Boolean = false
 
-  /**
-    * Appelée lors de la réception d'un événement écouté par la projection.
-    *
-    * @return ReceiveEvent: le Future[Unit] indiquant un side effect sur la projection
-    */
   override def onEvent: ReceiveEvent = {
     case e: CandidatInscritEvent => onCandidatInscritEvent(e)
   }
