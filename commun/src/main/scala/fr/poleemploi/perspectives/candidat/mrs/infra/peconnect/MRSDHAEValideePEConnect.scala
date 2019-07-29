8576fcb53d67a496021f6ff7f631d0b375751901
Brice Friederich <friederich.brice@gmail.com>
2019-07-29 17:14:48 +0200

    nettoyage commentaire

--- commun/src/main/scala/fr/poleemploi/perspectives/candidat/mrs/infra/peconnect/MRSDHAEValideePEConnect.scala
+++ commun/src/main/scala/fr/poleemploi/perspectives/candidat/mrs/infra/peconnect/MRSDHAEValideePEConnect.scala
@@ -7,8 +7,7 @@ import fr.poleemploi.perspectives.commun.infra.peconnect.PEConnectId
 
 /**
   * DHAE (Des habiletés à l'emploi), est un projet de PôleEmploi afin de valider des candidats sur des CodeROME mais sans
-  * employeur derrière comme pour une MRS classique. <br />
-  * Les habiletés ne sont pas les mêmes que pour les MRS.
+  * employeur derrière comme pour une MRS classique.
   */
 case class MRSDHAEValideePEConnect(peConnectId: PEConnectId,
                                    codeROME: CodeROME,
