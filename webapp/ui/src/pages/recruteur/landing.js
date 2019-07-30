0ea260558be5ca8060f7c6e9fc4d899e88886f6a
Brice Friederich <friederich.brice@gmail.com>
2019-07-30 10:21:49 +0200

    - nouvelle landing recruteur  - simplification CSS des landings candidat et recruteur  - ajout d'un meta pour le layout recruteur

--- webapp/ui/src/pages/recruteur/landing.js
+++ webapp/ui/src/pages/recruteur/landing.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import $ from 'jquery';
+import 'bootstrap/js/dist/modal';
 import Temoignages from '../../composants/Temoignages.vue';
 
 new Vue({
@@ -17,7 +19,21 @@ new Vue({
                     source: "Matthieu V. - Manufacture Baizet",
                     texte: "Les profils correspondent, en termes d'aptitudes, aux profils que je recherche. Cela me permet d'être plus efficient. Je suis très satisfait de votre service."
                 }
-            ]
+            ],
+            urlVideoEmission: 'https://www.youtube.com/embed/IkCyCvJxGTo?autoplay=1',
+            urlVideoMRS: 'https://www.youtube.com/embed/VpQOnDxUQek?ecver=2&autoplay=1'
         }
+    },
+    mounted: function () {
+        var modaleVideoYoutube = $("#js-modaleVideo");
+        var videoMRSYoutube = $("#js-videoMRSYoutube");
+
+        modaleVideoYoutube.on("show.bs.modal", function (event) {
+            var button = $(event.relatedTarget);
+            videoMRSYoutube.attr("src", button.attr('data-urlVideo'));
+        });
+        modaleVideoYoutube.on("hidden.bs.modal", function () {
+            videoMRSYoutube.attr("src", "");
+        });
     }
 });
