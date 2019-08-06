db210ff3c602ed4d286cd5073eee6e001cd1bfc6
Brice Friederich <friederich.brice@gmail.com>
2019-08-06 16:01:56 +0200

    refacto Action Play pour utiliser Results directement

--- webapp/app/authentification/infra/play/ConseillerAction.scala
+++ webapp/app/authentification/infra/play/ConseillerAction.scala
@@ -3,7 +3,6 @@ package authentification.infra.play
 import fr.poleemploi.perspectives.authentification.domain.ConseillerAuthentifie
 import fr.poleemploi.perspectives.conseiller.{AutorisationService, ConseillerId, RoleConseiller}
 import javax.inject.Inject
-import play.api.http.Status.UNAUTHORIZED
 import play.api.mvc._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -18,13 +17,13 @@ class ConseillerAdminAuthentifieAction @Inject()(override val parser: BodyParser
                                                  sessionConseillerAuthentifie: SessionConseillerAuthentifie,
                                                  autorisationService: AutorisationService)
                                                 (implicit val executionContext: ExecutionContext)
-  extends ActionBuilder[ConseillerAuthentifieRequest, AnyContent] {
+  extends ActionBuilder[ConseillerAuthentifieRequest, AnyContent] with Results {
 
   override def invokeBlock[A](request: Request[A], block: ConseillerAuthentifieRequest[A] => Future[Result]): Future[Result] = {
     sessionConseillerAuthentifie
       .get(request.session)
       .flatMap(c => if (autorisationService.hasRole(c.conseillerId, RoleConseiller.ADMIN)) Some(c) else None)
       .map(c => block(ConseillerAuthentifieRequest(c, request)))
-      .getOrElse(Future.successful(Results.Status(UNAUTHORIZED)))
+      .getOrElse(Future.successful(Unauthorized))
   }
 }
