c1085fa45adadbb0004c5c7f98cfe9236b5ea89c
Brice Friederich <friederich.brice@gmail.com>
2019-09-02 10:19:09 +0200

    nettoyage FIXME

--- commun/src/main/scala/fr/poleemploi/perspectives/commun/domain/Habilete.scala
+++ commun/src/main/scala/fr/poleemploi/perspectives/commun/domain/Habilete.scala
@@ -2,5 +2,4 @@ package fr.poleemploi.perspectives.commun.domain
 
 import fr.poleemploi.eventsourcing.StringValueObject
 
-// FIXME : en fait il faut faire un Code à part : le label sera dans HabileteDTO
 case class Habilete(value: String) extends StringValueObject
