832f4ec018a02fafad5b52cad397b567e2f57d63
mbarroux <mickael.barroux@gmail.com>
2019-09-02 17:35:47 +0200

    Utilisation des secrets docker-swarm pour injecter les donnees sensibles dans les variables d'environnement utilisees dans la conf de l'appli Play
    Pour deployer la stack en precisant la version :
    
    BATCHS_VERSION=1.0.32-SNAPSHOT; export BATCHS_VERSION; WEBAPP_VERSION=1.0.32-SNAPSHOT; export WEBAPP_VERSION; sudo -E docker stack deploy --compose-file docker-cloud.yml -c docker-cloud.recette.yml perspectives

---
+++ docker/webapp_entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# export Docker Swarm secrets to environment variables required by Play application
+export ALGOLIA_PLACES_APIKEY=$(cat "${ALGOLIA_PLACES_APIKEY_FILE}")
+export ALGOLIA_PLACES_APPID=$(cat "${ALGOLIA_PLACES_APPID_FILE}")
+export OAUTH2_CLIENT_ID=$(cat "${OAUTH2_CLIENT_ID_FILE}")
+export OAUTH2_CLIENT_SECRET=$(cat "${OAUTH2_CLIENT_SECRET_FILE}")
+export MAILJET_APIKEY_PUBLIC=$(cat "${MAILJET_APIKEY_PUBLIC_FILE}")
+export MAILJET_APIKEY_PRIVATE=$(cat "${MAILJET_APIKEY_PRIVATE_FILE}")
+export AUTOLOGIN_SECRET_KEY=$(cat "${AUTOLOGIN_SECRET_KEY_FILE}")
+export POSTGRES_PASSWORD=$(cat "${POSTGRES_PASSWORD_FILE}")
+export PLAY_HTTP_SECRET_KEY=$(cat "${PLAY_HTTP_SECRET_KEY_FILE}")
+export GOOGLE_TAG_MANAGER_CONTAINER_ID=$(cat "${GOOGLE_TAG_MANAGER_CONTAINER_ID_FILE}")
+export SLACK_WEBHOOK_URL=$(cat "${SLACK_WEBHOOK_URL_FILE}")
+
+sh -c "./perspectives-webapp-${WEBAPP_VERSION}/bin/perspectives-webapp -Dlogger.resource=${WEBAPP_LOGBACK_RESOURCE} -Dconfig.resource=${WEBAPP_CONFIG_RESOURCE}"
